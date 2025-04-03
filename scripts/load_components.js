1afa5a0d2ef047f8dc8f073b608b9a3980d326c1
Shay Sluiter <Captn.XP@hotmail.com>
2025-04-03 13:05:29 -0500

    Implemented recursive includes. Basic recursion halting (no linking self in include file). Moved child filter code to separate method `filterChildren`.

--- scripts/load_components.js
+++ scripts/load_components.js
@@ -12,16 +12,18 @@ const rgxParams = /\$\{param\.[\w]+\}/ig;
 const getParamRegex = (name) => new RegExp(`\\$\\{param\\.${name}\\}`,'i');
 
 const replaceElement = (from, to) => {
-	try { from.parentNode.replaceChild(to,from); }
+	try { from.parentNode.replaceChild(to, from); }
 	catch (error) {
 		console.error(error);
 		throw error;
 	}
 };
 
+const filterChildren = (element, selector) => [...element.children].filter(elm => elm.matches(selector));
+
 const includeHTML = (element) => {
 	//? Get param tags within include element.
-	const paramTags = [...element.children].filter(elm => elm.matches("param"));
+	const paramTags = filterChildren(element, "param");
 	//? Create an object out of the params' names and values.
 	//TODO: Might look into shorter way of achieving this for later indexing.
 	const params = Object.fromEntries(paramTags.map((elm) => [elm.getAttribute("name"), elm.value]));
@@ -37,18 +39,26 @@ const includeHTML = (element) => {
 				if (!rgxTest.test(element.innerHTML)) return;
 				element.innerHTML = element.innerHTML.replace(rgxTest,params[key]);
 			});
+			//? Add includes recursively.
+			const firstChild = element.children[0];
+			const subIncludes = filterChildren(firstChild, "include");
+			if (subIncludes) { includeMain(subIncludes, filePath); } // Make async to not be slow.
 			//? Replace the include element with the file contents.
 			//TODO: currently can only use one contained element in files.
 			//TODO: will look for solution to include whole file contents later.
-			replaceElement(element, element.children[0]);
+			replaceElement(element, firstChild);
 		})
 		.catch(error => console.error(`Error loading '${filePath}': `, error));
 };
 
+const includeMain = (includes, filePath) => {
+	for (const include of includes) {
+		if (include.getAttribute("src") == filePath) continue;
+		includeHTML(include);
+	}
+};
+
 document.addEventListener("DOMContentLoaded", () => {
 	const includes = getElements("include");
-	for (const include of includes) {
-		includeHTML(include);
-		include.textContent.replaceAll(rgxWhitespace,"");
-	}
+	includeMain(includes)
 });
