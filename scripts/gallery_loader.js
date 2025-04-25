a5277e68b12942ea5c8c0930b1f29c44091ccc01
JelleyStatic <captn.XP@hotmail.com>
2025-04-25 17:14:14 -0500

    Created script for loading galleries with a json file.

---
+++ scripts/gallery_loader.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const loadJson = async filePath => (await fetch(filePath)).json();
+
+const loadGalleries = async() => {
+	const galleries = getElements(".gallery");
+	//
+	for (const gallery of galleries) {
+		const gallerySrc = gallery.getAttribute("id");
+		const galleryEntries = await loadJson(`assets/${gallerySrc}.json`);
+		//
+		for (const entry of galleryEntries) {
+			const baseDiv = document.createElement("div");
+			const img = document.createElement("img");
+			const caption = document.createElement("label");
+			//
+			img.id = entry.id;
+			img.src = entry.image_link;
+			img.alt = entry.alt_text;
+			caption.for = entry.id;
+			caption.textContent = entry.caption;
+			//
+			baseDiv.appendChild(img);
+			baseDiv.appendChild(caption);
+			//
+			gallery.appendChild(baseDiv);
+		}
+	}
+};
+
+document.addEventListener("DOMContentLoaded", loadGalleries);
