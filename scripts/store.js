befab9d0cadccd6cb40d4d0e1abc4377bf48f33a
JelleyStatic <captn.XP@hotmail.com>
2025-04-25 21:12:42 -0500

    Redesigned store page.

---
+++ scripts/store.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const initializeCart = () => {
+	if (!sessionStorage.getItem("cart"))
+		sessionStorage.setItem("cart", "{ \"items\":[] }");
+};
+
+const addToCart = (itemId) => {
+	let cart = JSON.parse(sessionStorage.cart);
+	// if (cart.items.includes(item)) return;
+	cart.items.push(itemId);
+	sessionStorage.cart = JSON.stringify(cart);
+};
+
+const cart = (evt) => {
+	evt.preventDefault()
+	addToCart(getElement("input[name=item_id]").value);
+	getElement("#cart_dialog").showModal();
+};
+
+const goToCart = () => getElement("form").submit();
+const closeDialog = () => getElement("#cart_dialog").close();
+
+document.addEventListener("DOMContentLoaded", (evt) => {
+	initializeCart();
+	getElement("form").addEventListener("submit", cart);
+	getElement("#dialog_cart").addEventListener("click", goToCart)
+	getElement("#dialog_close").addEventListener("click", closeDialog);
+});
