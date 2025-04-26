6f95f8e57061b87c6c37e68fecf5f35a16186a68
JelleyStatic <captn.XP@hotmail.com>
2025-04-25 20:05:33 -0500

    Split up loop contents into methods.

--- scripts/price_tag.js
+++ scripts/price_tag.js
@@ -11,26 +11,34 @@ const USD = new Intl.NumberFormat("en-US", { style: "currency", currency:"USD" }
 
 const formatPercentage = number => Math.abs(number).toLocaleString("en-US", { maximumFractionDigits: 0 }) + `% ${number > 0 ? "ON" : "OFF"}`;
 
+const getSaleAmount = (basePrice, salePrice) => ((salePrice - basePrice) / basePrice) * 100;
+
+const buildPriceElement = (priceElement) => {
+	const basePrice = parseFloat(priceElement.getAttribute("base_price"));
+	const salePrice = parseFloat(priceElement.getAttribute("sale_price"));
+	const salePercentage = getSaleAmount(basePrice, salePrice);
+	//
+	const originalPriceSpan = createElementWithText("div", USD.format(basePrice));
+	originalPriceSpan.setAttribute("class", "base_price");
+	priceElement.appendChild(originalPriceSpan);
+	//
+	const salePriceSpan = createElementWithText("div", USD.format(salePrice));
+	if (isNaN(salePrice)) return;
+	const saleSpan = createElementWithText("div", formatPercentage(salePercentage));
+	//
+	salePriceSpan.setAttribute("class", "sale_price");
+	saleSpan.setAttribute("class", "sale_amount");
+	//
+	priceElement.appendChild(salePriceSpan);
+	priceElement.appendChild(saleSpan);
+};
+
 const addPrices = () => {
 	const priceElms = getElements(".price");
 	for (const priceElm of priceElms) {
 		const basePrice = parseFloat(priceElm.getAttribute("base_price"));
 		if (isNaN(basePrice)) continue;
-		const salePrice = parseFloat(priceElm.getAttribute("sale_price"));
-		const salePercentage = ((salePrice - basePrice) / basePrice) * 100;
-		//
-		const originalPriceSpan = createElementWithText("div", USD.format(basePrice));
-		const salePriceSpan = createElementWithText("div", USD.format(salePrice));
-		const saleSpan = createElementWithText("div", formatPercentage(salePercentage));
-		//
-		originalPriceSpan.setAttribute("class", "base_price");
-		salePriceSpan.setAttribute("class", "sale_price");
-		saleSpan.setAttribute("class", "sale_amount");
-		//
-		priceElm.appendChild(originalPriceSpan);
-		if (isNaN(salePrice)) continue;
-		priceElm.appendChild(salePriceSpan);
-		priceElm.appendChild(saleSpan);
+		buildPriceElement(priceElm);
 	}
 };
 
