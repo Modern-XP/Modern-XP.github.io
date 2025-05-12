8087561631f2ec7f8294cbc676e9e49b75fda509
JelleyStatic <captn.XP@hotmail.com>
2025-05-11 23:17:18 -0500

    Moved translate string calculation to separate function.

--- scripts/gallery_loader.js
+++ scripts/gallery_loader.js
@@ -2,6 +2,15 @@
 
 const loadJson = async filePath => (await fetch(filePath)).json();
 
+const calculateTranslateString = (galleryColumnCount, entryCount) => {
+	const remainder = entryCount % galleryColumnCount;
+	//
+	const halfWidth = (galleryColumnCount - 1) / 2;
+	const offsetMultiplier = 1 - ((remainder - 1) / Math.max(1,galleryColumnCount - 1));
+	//
+	return `translate: calc((100% + 2ch) * ${halfWidth} * ${offsetMultiplier}) 0;`;
+};
+
 const loadGalleries = async() => {
 	const galleries = getElements(".gallery");
 	//
@@ -10,15 +19,10 @@ const loadGalleries = async() => {
 		const galleryEntries = await loadJson(`assets/${gallerySrc}.json`);
 		//
 		const calculatedGallery = window.getComputedStyle(gallery);
-		//
 		const galleryColumnCount = calculatedGallery.getPropertyValue("grid-template-columns").split(" ").length;
 		const galleryResizeRowIndex = Math.floor(galleryEntries.length / galleryColumnCount);
-		const galleryRemainder = galleryEntries.length % galleryColumnCount;
 		//
-		const halfWidth = (galleryColumnCount - 1) / 2;
-		const offsetMultiplier = 1 - ((galleryRemainder - 1) / Math.max(1,galleryColumnCount - 1));
-		//
-		const translateString = `translate: calc((100% + 2ch) * ${halfWidth} * ${offsetMultiplier}) 0;`;
+		const translateString = calculateTranslateString(galleryColumnCount, galleryEntries.length);
 		//
 		for (const i in galleryEntries) {
 			const entry = galleryEntries[i]
